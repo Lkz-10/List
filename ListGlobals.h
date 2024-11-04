56e0325f4f695fa72dcff6f39969bcba85a90587
Lkz-10 <gkazarin.10@bk.ru>
2024-11-05 01:09:31 +0300

    Verification into debug mode!

--- ListGlobals.h
+++ ListGlobals.h
@@ -3,22 +3,22 @@
 
 #define DEBUG
 
+typedef int list_elem_t;
+
 #ifdef DEBUG
     #define ON_DEBUG(...) __VA_ARGS__
+    #define LIST_VERIFY                                \
+        if (ListVerify(list) != 0) return -1;
 #else
     #define ON_DEBUG(...)
+    #define LIST_VERIFY
 #endif
-
-#define LIST_VERIFY                                    \
-    if (ListVerify(list) != 0) return -1;
 
 #define INSERT(index, val)                             \
     if (ListInsert(&list, index, val) != 0) return -1;
 
 #define ERASE(index)                                   \
     if (ListErase(&list, index) != 0) return -1;
-
-typedef int list_elem_t;
 
 const list_elem_t POISON        = -111111;
 const int         LIST_MIN_SIZE = 2;
